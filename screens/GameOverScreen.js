0bdaf6a252596cdff2b04ea66c57be08cbb0b64d
adclleva <arvin.lleva@yahoo.com>
2020-06-07 17:40:01 -0700

    Was able to calculate the sizes dynamically for the game overscreen and have it shown properly on smaller devices

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,5 +1,13 @@
 import React from "react";
-import { StyleSheet, Text, View, Button, Image } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  Image,
+  Dimensions,
+  ScrollView,
+} from "react-native";
 import BodyText from "../components/BodyText";
 import TitleText from "../components/TitleText";
 import Colors from "../constants/colors";
@@ -8,30 +16,32 @@ import MainButton from "../components/MainButton";
 const GameOverScreen = (props) => {
   const { roundsNumber, userNumber, onRestart } = props;
   return (
-    <View style={styles.screen}>
-      <TitleText>The Game is Over!</TitleText>
-      <View style={styles.imageContainer}>
-        <Image
-          // the fade-in effect is ued for all images when loading
-          fadeDuration={100}
-          // source={require("../assets/success.png")}
-          source={{
-            uri:
-              "https://cdn.pixabay.com/photo/2019/01/22/18/30/summit-3948706_960_720.jpg",
-          }}
-          style={styles.image}
-          resizeMode="cover"
-        />
+    <ScrollView>
+      <View style={styles.screen}>
+        <TitleText>The Game is Over!</TitleText>
+        <View style={styles.imageContainer}>
+          <Image
+            // the fade-in effect is ued for all images when loading
+            fadeDuration={100}
+            // source={require("../assets/success.png")}
+            source={{
+              uri:
+                "https://cdn.pixabay.com/photo/2019/01/22/18/30/summit-3948706_960_720.jpg",
+            }}
+            style={styles.image}
+            resizeMode="cover"
+          />
+        </View>
+        <View style={styles.resultContainer}>
+          <BodyText style={styles.resultText}>
+            Your phone needed{" "}
+            <Text style={styles.highlight}>{roundsNumber}</Text> rounds to guess
+            the number <Text style={styles.highlight}>{userNumber}</Text>
+          </BodyText>
+        </View>
+        <MainButton onPress={onRestart}>NEW GAME</MainButton>
       </View>
-      <View style={styles.resultContainer}>
-        <BodyText style={styles.resultText}>
-          Your phone needed{" "}
-          <Text style={styles.hightlight}>{roundsNumber}</Text> rounds to guess
-          the number <Text style={styles.hightlight}>{userNumber}</Text>
-        </BodyText>
-      </View>
-      <MainButton onPress={onRestart}>NEW GAME</MainButton>
-    </View>
+    </ScrollView>
   );
 };
 
@@ -46,29 +56,29 @@ const styles = StyleSheet.create({
      * having the same height and width and also halving the borderRadius
      *
      */
-    borderRadius: 150,
+    borderRadius: (Dimensions.get("window").width * 0.7) / 2,
     borderWidth: 3,
     borderColor: "black",
-    width: 300,
-    height: 300,
+    width: Dimensions.get("window").width * 0.7,
+    height: Dimensions.get("window").width * 0.7,
     overflow: "hidden", // The overflow is clipped, the rest of the content will be invisible
-    marginVertical: 30,
+    marginVertical: Dimensions.get("window").height / 30,
   },
   image: {
     width: "100%",
     height: "100%",
   },
-  hightlight: {
+  highlight: {
     color: Colors.primary,
     fontFamily: "open-sans-bold",
   },
   resultContainer: {
     marginHorizontal: 30,
-    marginVertical: 15,
+    marginVertical: Dimensions.get("window").height / 60,
   },
   resultText: {
     textAlign: "center",
-    fontSize: 20,
+    fontSize: Dimensions.get("window").width < 400 ? 16 : 20,
   },
 });
 
